d30e147ad15c7f5792ab158ef5d461dc64b779e2
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-05-25 14:44:35 +0200

    Adds react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Checkout from "./pages/Checkout";
+import CheckoutSuccess from "./pages/CheckoutSuccess";
+import RouteNotFound from "./pages/RouteNotFound";
+
 export default function App() {
   return (
     <div>
-      <h1>online-store</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkoutSuccess" element={<CheckoutSuccess />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
