f03d1304596d1775b3caaec18f68eb29751b55df
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-05-26 19:01:51 +0200

    Adds checkout success page content

--- src/pages/CheckoutSuccess/index.jsx
+++ src/pages/CheckoutSuccess/index.jsx
@@ -1,3 +1,20 @@
+import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { CartLengthContext } from "../../components/Layout";
+
 export default function CheckoutSuccess() {
-  return <div>CheckoutSuccess</div>;
+  localStorage.clear();
+  const { setItemsInCart } = useContext(CartLengthContext);
+
+  useEffect(() => {
+    setItemsInCart(0);
+  }, []);
+
+  return (
+    <div>
+      <h1>Order was successful</h1>
+      <Link to="/">Back to store</Link>
+    </div>
+  );
 }
