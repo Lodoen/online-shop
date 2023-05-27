7bd9ae90b8619f42c6ef4c093112c24762bbe40f
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-05-27 22:06:37 +0200

    Adds cart icon

--- src/components/ui/CartIcon/index.jsx
+++ src/components/ui/CartIcon/index.jsx
@@ -1,9 +1,18 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { RiShoppingCartFill } from "react-icons/ri";
 
 import { CartLengthContext } from "../../Layout";
+import { CartIconContainer } from "./index.styles";
 
 export default function CartIcon() {
   const { itemsInCart } = useContext(CartLengthContext);
-  return <Link to="/checkout">CART ICON ({itemsInCart})</Link>;
+  return (
+    <CartIconContainer>
+      <Link to="/checkout">
+        <RiShoppingCartFill />
+        <span>( {itemsInCart} )</span>
+      </Link>
+    </CartIconContainer>
+  );
 }
